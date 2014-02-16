8c8ae24cd988a2756cd54af8025f9e7c1e63b0b1
shoito <infy2c@gmail.com>
2014-02-16 21:37:23 +0900

    ベース部分をリファクタリング

--- app/scripts/typetalk.js
+++ app/scripts/typetalk.js
@@ -4,14 +4,37 @@
     var Typetalk = (function() {
         var storage = chrome.storage.local;
 
-        function Typetalk(clientId, clientSecret) {
+        function Typetalk(clientId, clientSecret, requestTimeout) {
             this.clientId = clientId;
             this.clientSecret = clientSecret;
+            this.requestTimeout = requestTimeout || 3000;
+
             this.redirectUri = 'https://' + chrome.runtime.id + '.chromiumapp.org/provider_cb';
             this.accessToken = null;
             this.refreshToken = null;
 
             this.loadTokens();
+        }
+
+        Typetalk.prototype.createTimeout = function(xhr, requestTimeout) {
+            return setTimeout(function() {
+                if (xhr.readyState < 4) {
+                    xhr.abort();
+                }
+            }, requestTimeout);
+        }
+
+        Typetalk.prototype.validateAccessTokenSync = function() {
+            if (!this.accessToken) return false;
+
+            var xhr = new XMLHttpRequest(),
+                timerId = this.createTimeout(xhr, this.requestTimeout);
+            xhr.open('GET', 'https://typetalk.in/api/v1/profile', false);
+            xhr.setRequestHeader('Authorization', 'Bearer ' + this.accessToken);
+            xhr.send(null);
+            clearTimeout(timerId);
+
+            return (xhr.status === 200);
         }
 
         Typetalk.prototype.hasTokens = function() {
@@ -24,7 +47,8 @@
                 function(tokens) {
                     if(chrome.extension.lastError !== undefined) {
                         console.error(chrome.extension.lastError);
-                        self.accessToken = self.refreshToken = null;
+                        self.accessToken = null;
+                        self.refreshToken = null;
                     } else {
                         self.accessToken = tokens.access_token;
                         self.refreshToken = tokens.refresh_token;
@@ -41,13 +65,15 @@
                             + '&scope=topic.read,topic.post,my&response_type=code',
                     'interactive':true}, 
                 function(authorizeResponse) {
-                    var code = authorizeResponse.match("code=(.+)")[1];
+                    var code = authorizeResponse.match('code=(.+)')[1];
                     self.getAccessToken(code);
                 }
             );
         };
 
         Typetalk.prototype.getAccessToken = function(code, callback) {
+            if (!code) return callback && callback(null, null);
+
             var param = 'client_id=' + this.clientId 
                         + '&client_secret=' + this.clientSecret
                         + '&redirect_uri=' + this.redirectUri
@@ -60,27 +86,54 @@
             var self = this,
                 xhr = new XMLHttpRequest();
             xhr.onreadystatechange = function() {
-                if(xhr.readyState == 4) {
-                    if (xhr.status == 200) {
+                if(xhr.readyState === 4) {
+                    if (xhr.status === 200) {
                         var resJson = JSON.parse(xhr.responseText);
                         self.saveTokens(resJson.access_token, resJson.refresh_token);
-                        callback(null, resJson);
+                        callback && callback(resJson, xhr);
                     } else {
-                        callback(xhr.status, null);
+                        callback && callback(null, xhr);
                     }
                 }
             };
             xhr.open('POST', 'https://typetalk.in/oauth2/access_token');
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.timeout = this.requestTimeout;
             xhr.send(param);
         };
 
         Typetalk.prototype.refreshAccessToken = function(callback) {
+            if (!this.refreshToken) return callback && callback(null, null);
+
             var param = 'client_id=' + this.clientId 
                         + '&client_secret=' + this.clientSecret
                         + '&grant_type=refresh_token'
                         + '&refresh_token=' + this.refreshToken;
             this.postToAccessTokenEndpoint(param, callback);
+        };
+
+        Typetalk.prototype.refreshAccessTokenSync = function() {
+            if (!this.refreshToken) return false;
+
+            var xhr = new XMLHttpRequest(),
+                timerId = this.createTimeout(xhr, this.requestTimeout),
+                param = 'client_id=' + this.clientId 
+                        + '&client_secret=' + this.clientSecret
+                        + '&grant_type=refresh_token'
+                        + '&refresh_token=' + this.refreshToken;
+            xhr.open('POST', 'https://typetalk.in/oauth2/access_token', false);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.send(param);
+            clearTimeout(timerId);
+
+            if (xhr.status === 200) {
+                var resJson = JSON.parse(xhr.responseText);
+                this.saveTokens(resJson.access_token, resJson.refresh_token);
+                return true;
+            } else {
+                this.clearTokens();
+                return false;
+            }
         };
 
         Typetalk.prototype.saveTokens = function(accessToken, refreshToken) {
@@ -105,39 +158,45 @@
         };
 
         Typetalk.prototype.getNotifications = function(callback) {
+            if (!this.accessToken) return callback && callback(null, null);
+
             var self = this,
                 xhr = new XMLHttpRequest();
             xhr.onreadystatechange = function() {
-                if(xhr.readyState == 4) {
-                    if (xhr.status == 200) {
+                if(xhr.readyState === 4) {
+                    if (xhr.status === 200) {
                         var resJson = JSON.parse(xhr.responseText);
-                        callback(null, resJson);
+                        callback && callback(resJson, xhr);
                     } else {
-                        callback(xhr.status, null);
+                        callback && callback(null, xhr);
                     }
                 }
             };
             xhr.open('GET', 'https://typetalk.in/api/v1/notifications/status');
             xhr.setRequestHeader('Authorization', 'Bearer ' + this.accessToken);
-            xhr.send();
+            xhr.timeout = this.requestTimeout;
+            xhr.send(null);
         };
 
         Typetalk.prototype.readNotifications = function(callback) {
+            if (!this.accessToken) return callback && callback(null, null);
+
             var self = this,
                 xhr = new XMLHttpRequest();
             xhr.onreadystatechange = function() {
-                if(xhr.readyState == 4) {
-                    if (xhr.status == 200) {
+                if(xhr.readyState === 4) {
+                    if (xhr.status === 200) {
                         var resJson = JSON.parse(xhr.responseText);
-                        callback(null, resJson);
+                        callback && callback(resJson, xhr);
                     } else {
-                        callback(xhr.status, null);
+                        callback && callback(null, xhr);
                     }
                 }
             };
             xhr.open('PUT', 'https://typetalk.in/api/v1/notifications/open');
             xhr.setRequestHeader('Authorization', 'Bearer ' + this.accessToken);
-            xhr.send();
+            xhr.timeout = this.requestTimeout;
+            xhr.send(null);
         };
 
         return Typetalk;
